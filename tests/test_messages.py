a1a66d90e9c187fe34894f8d3c859d10e34c3902
Nathan Bergey <nathan.bergey@gmail.com>
2014-03-11 14:12:43 -0700

    too long test

--- tests/test_messages.py
+++ tests/test_messages.py
@@ -62,6 +62,11 @@ class TestMessages(unittest.TestCase):
         raw = b'\x08\x13\x00\x00\x00\x00\x00\x14\xfe\xda\x00\x00\x00'
         self.assertRaises(messages.MessageSizeError, messages.ADIS.decode, raw)
 
+    def test_decode_too_long(self):
+        raw = b'\x08\x13\x00\x00\x00\x00\x00\x14\xfe\xda\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\xff\xdd\x00\x00\x00'
+        self.assertRaises(messages.MessageSizeError, messages.ADIS.decode, raw)
+
+
     def test_typedef(self):
 
         self.maxDiff = None
